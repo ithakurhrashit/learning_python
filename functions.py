f3d13541e5df90a64b6ca377a91893a35a088e12
ithakurhrashit <25116cn362@glbitm.ac.in>
2026-06-23 11:10:35 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,15 @@
+# # def gmean(a,b):
+# #     mean=((a*b)/(a+b))
+# #     print(mean)
+
+# # c=int(input("enter the first number"))
+# # d=int(input("enter the second number"))
+# # gmean(c,d)
+
+# def average(*numbers):
+#     sum =0
+#     for i in numbers:
+#        sum = sum +i
+#     print("the avegrage is " , sum/len(numbers))
+
+# average(6,7,8,9)
