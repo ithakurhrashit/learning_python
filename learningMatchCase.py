8530b51f86774aaa04ab182a8e7f72b86dd6a705
ithakurhrashit <25116cn362@glbitm.ac.in>
2026-06-23 11:11:11 +0530

    Add files via upload

---
+++ learningMatchCase.py
@@ -0,0 +1,16 @@
+# x=int(input("enter first number : "))
+# y=int(input("enter second number : "))
+# ch=input("enter any operator +/-/*// ")
+# match ch :
+#     case '+':
+#         print("result is : " , x+y)
+#     case '-':
+#         print("result is : " , x-y )
+#     case '*':
+#         print("result is : " , x*y)
+#     case '/':
+#         print("result is : " , x/y)
+#     case _:
+#         print("operator not found ")
+name="harshit"
+print(name)
